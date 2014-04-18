785b9dc45932058d81bb53bea4fa5b4175207602
andresbravog <andresbravog@gmail.com>
2014-04-18 15:49:27 -0700

    Fixed weird where in controllers

--- spec/dummy/app/controllers/comments_controller.rb
+++ spec/dummy/app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   after_filter only: [:index] { set_pagination_headers(:comments) }
 
   def index
-    @comments = Comment.where('id > 0').paginate(per_page: params[:per_page], page: params[:page])
+    @comments = Comment.paginate(per_page: params[:per_page], page: params[:page])
     respond_with @comments
   end
 end
